03ed146effa337cd09e115c92e9a95291e23437b
wrong89 <fenomen_03@mail.ru>
2023-10-10 02:08:41 +0300

    Error page and styles

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import Layout from "./components/Layout/Layout";
 import Main from "./pages/Main";
 import {Route, Routes} from "react-router-dom";
 import History from "./pages/History";
+import Error from "./pages/Error";
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
                 <Routes>
                     <Route path={'/'} element={<Main/>}/>
                     <Route path={'/history'} element={<History/>}/>
+                    <Route path={'*'} element={<Error/>}/>
                 </Routes>
             </Layout>
         </div>
